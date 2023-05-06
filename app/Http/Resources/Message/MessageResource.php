5bda8cc23c8acebb787d7086164d526bb3478ce0
Islam Bilalov <bilalov1504@gmail.com>
2023-05-06 13:51:01 +0300

    feat: create message resource

---
+++ app/Http/Resources/Message/MessageResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Message;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed $id
+ * @property mixed $body
+ * @property mixed $created_at
+ */
+class MessageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'body' => $this->body,
+            'time' => $this->created_at->diffForHumans(),
+        ];
+    }
+}
