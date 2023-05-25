dbfcc2556e191f73c5f88452585c462cc1ba8fcc
Islam Bilalov <bilalov1504@gmail.com>
2023-05-25 11:56:03 +0300

    feat: create foreign id for user_id

--- database/migrations/2023_05_02_142036_create_messages_table.php
+++ database/migrations/2023_05_02_142036_create_messages_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,6 +15,12 @@ return new class extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate()
+            ;
 
             $table->text('body');
         });
