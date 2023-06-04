ad9eee48cbdd5a0239d255f88b0aac9d458e645d
Islam Bilalov <bilalov1504@gmail.com>
2023-06-04 20:06:55 +0300

    feat: add authencticated layout

--- resources/js/Pages/Message/Messages.vue
+++ resources/js/Pages/Message/Messages.vue
@@ -1,58 +1,68 @@
 <template>
-    <div class="w-1/2 mx-auto py-6">
-        <h1 class="mb-4 text-center">Enter your message</h1>
+    <AuthenticatedLayout>
+        <div class="py-4 max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="w-1/2 mx-auto py-6">
+                    <h1 class="mb-4 text-center">Enter your message</h1>
 
-        <div>
-            <div class="mb-12">
-                <form>
                     <div>
-                        <input
-                            v-model="body"
-                            type="text"
-                            class="mb-2 w-full rounded-lg border border-gray-400"
-                            placeholder="Message..."
-                        >
+                        <div class="mb-12">
+                            <form>
+                                <div>
+                                    <input
+                                        v-model="body"
+                                        type="text"
+                                        class="mb-2 w-full rounded-lg border border-gray-400"
+                                        placeholder="Message..."
+                                    >
+                                </div>
+
+                                <div class="text-right">
+                                    <button
+                                        @click.prevent="store"
+                                        class="bg-blue-600 hover:bg-sky-500 text-white py-2 px-4 rounded-lg"
+                                        type="submit"
+                                    >
+                                        Send
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
 
-                    <div class="text-right">
-                        <button
-                            @click.prevent="store"
-                            class="bg-blue-600 hover:bg-sky-500 text-white py-2 px-4 rounded-lg"
-                            type="submit"
-                        >
-                            Send
-                        </button>
+                    <div v-if="messages.length > 0" class="mb-4">
+                        <h1 class="text-center mb-4">Messages</h1>
+
+                        <div>
+                            <div class="text-sm pb-4 mb-2 border-b border-gray-300" v-for="message in messages">
+                                <p
+                                    class="text-right"
+                                    v-if="message.user_id === this.$page.props.auth.user.id"
+                                >
+                                    {{ message.body }}
+                                </p>
+                                <p v-else>
+                                    {{ message.body }}
+                                </p>
+                                <p class="text-right text-gray-400">{{ message.time }}</p>
+                            </div>
+                        </div>
                     </div>
-                </form>
-            </div>
-        </div>
-
-        <div v-if="messages.length > 0" class="mb-4">
-            <h1 class="text-center mb-4">Messages</h1>
-
-            <div>
-                <div class="text-sm pb-4 mb-2 border-b border-gray-300" v-for="message in messages">
-                    <p
-                        class="text-right"
-                        v-if="message.user_id === this.$page.props.auth.user.id"
-                    >
-                        {{ message.body }}
-                    </p>
-                    <p v-else>
-                        {{ message.body }}
-                    </p>
-                    <p class="text-right text-gray-400">{{ message.time }}</p>
                 </div>
             </div>
         </div>
-    </div>
+
+    </AuthenticatedLayout>
+
 </template>
 
 <script>
 
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 
 export default {
     name: "Messages",
+    components: {AuthenticatedLayout},
 
     props: {
         messages: {
