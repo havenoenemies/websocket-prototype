965fc9a471996b5d9d77d020d43b4add36321437
Islam Bilalov <bilalov1504@gmail.com>
2023-06-11 21:47:15 +0300

    feat: create message components

---
+++ resources/js/Components/Message/MessageList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div
+            class="text-sm pb-4 mb-2 border-b border-gray-300"
+            v-for="message in messages"
+        >
+            <MessageItem
+                :message="message"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import MessageItem from "@/Components/Message/MessageItem.vue";
+
+export default {
+    name: "MessageList",
+    components: {MessageItem},
+
+    props: {
+        messages: {
+            type: Array,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Message/ProfilePicture.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <button class="w-12 h-12 rounded-full bg-gray-400"></button>
+    </div>
+</template>
