71018cb2c687cdb03ac50de2d6ab9628c6278f6a
Islam Bilalov <bilalov1504@gmail.com>
2023-06-18 22:30:59 +0300

    refactor: separate messages

---
+++ resources/js/Components/Message/AuthedMessageItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ProfilePicture from "@/Components/Message/ProfilePicture.vue";
+
+defineProps({
+    body: {
+        type: String,
+    },
+});
+</script>
+
+<template>
+    <ProfilePicture />
+
+    <div class="font-medium pt-2">
+        {{ body }}
+    </div>
+</template>
+
