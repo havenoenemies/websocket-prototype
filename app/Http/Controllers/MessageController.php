17c07400e24e07577a36cb5b299772acfb9f2efe
Islam Bilalov <bilalov1504@gmail.com>
2023-08-31 20:17:07 +0300

    refacator: message controller refactor

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -23,10 +23,7 @@ class MessageController extends Controller
 
     public function store(StoreRequest $request): MessageResource
     {
-        $data = $request->validated();
-
-        $message = Message::create($data);
-
+        $message = Message::create($request->validated());
         broadcast(new StoreMessageEvent($message))->toOthers();
 
         return MessageResource::make($message);
